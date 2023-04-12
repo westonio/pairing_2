bb489834f0091de69e2dd85e3fc840f8700e0d46
Weston Schutt <wtschutt@gmail.com>
2023-04-12 14:38:10 -0600

    Add new elsif statements and conditions

--- plants.rb
+++ plants.rb
@@ -1,8 +1,14 @@
-gets_sunshine = true
-ounces_of_water = 10
+gets_sunshine = false
+ounces_of_water = 12.5
 
-if gets_sunshine == true && ounces_of_water >= 10
+if gets_sunshine == true && ounces_of_water > 20
+    puts "The plant is drowning! Dry it out."
+elsif gets_sunshine == true && ounces_of_water >= 10
     puts "The plant will thrive!"
+elsif gets_sunshine == false && ounces_of_water >= 10
+    puts "The plant will die if you do not give it more sunshine!"
+elsif gets_sunshine == true && ounces_of_water < 10
+    puts "The plant will die if you do not give it more water"
 else
-    puts "The plant will die"
+    puts "The plant will die!"
 end
